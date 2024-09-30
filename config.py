25a8fc511949b8337334b776dc465ca0314b4328
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-30 07:22:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+API_ID = "21740783"  # Replace with your Telegram API ID
+API_HASH = "a5dc7fec8302615f5b441ec5e238cd46"  # Replace with your Telegram API Hash
+BOT_TOKEN = "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU"  # Replace with your Telegram bot token
+USERNAME = "ComboAp.ps28@gmail.com"  # Replace with your Crunchyroll email
+PASSWORD = "AlexVentas223"  # Replace with your Crunchyroll password
